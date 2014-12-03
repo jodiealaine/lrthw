71cc0d9101ea5580b3002d3e4a42724a99609838
Jodie Alaine Parker <me@jodiealaine.com>
2014-12-03 12:08:28 +0000

    Exercise 5 - More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+name = 'Bob Parker'
+age = 21 
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actially that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# Control + Commant + G (Find/Find All Using Selection)
