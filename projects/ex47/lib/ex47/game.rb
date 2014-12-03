2f9d35b16c06cd0b187bc023ef63c1fcfda89898
Jodie Alaine Parker <me@jodiealaine.com>
2014-12-03 17:57:21 +0000

    Exercise 47 - Automated Testing

---
+++ projects/ex47/lib/ex47/game.rb
@@ -0,0 +1,17 @@
+class Room
+	def initialize name, description
+		@name = name
+		@description = description
+		@paths = {}
+	end
+
+	attr_reader :name, :paths, :description
+
+	def go direction
+		@paths[direction]
+	end
+
+	def add_paths paths
+		@paths.update paths
+	end
+end
